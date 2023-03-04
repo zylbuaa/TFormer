212008ca8da4edcef1e0c3206c709f7507d96595
zylbuaa <90943537+zylbuaa@users.noreply.github.com>
2023-03-04 22:47:56 +0800

    Update eval_metrics.py

--- src/eval_metrics.py
+++ src/eval_metrics.py
@@ -18,9 +18,9 @@ import torch
 class ConfusionMatrix(object):
 
     def __init__(self,num_classes:int,labels:list):
-        self.matrix=np.zeros((num_classes,num_classes)) #初始化混淆矩阵，元素都为0
-        self.num_classes=num_classes #类别数量
-        self.labels=labels #类别标签
+        self.matrix=np.zeros((num_classes,num_classes)) #initial matrix
+        self.num_classes=num_classes # classes num
+        self.labels=labels # classes labels
         self.PrecisionofEachClass=[0.0 for cols in range(self.num_classes)]
         self.SensitivityofEachClass=[0.0 for cols in range(self.num_classes)]
         self.SpecificityofEachClass=[0.0 for cols in range(self.num_classes)]
@@ -28,9 +28,11 @@ class ConfusionMatrix(object):
 
 
     def update(self,pred,label):
-        # for p,t in zip(preds, labels): #pred为预测结果，labels为真实标签
-        #     self.matrix[p,t] += 1 #根据预测结果和真实标签的值统计数量，在混淆矩阵相应的位置+1
-        self.matrix[pred,label] += 1
+        if len(pred)>1:
+            for p,t in zip(pred, label): 
+                self.matrix[int(p),int(t)] += 1 
+        else:
+            self.matrix[int(pred),int(label)] += 1
 
     def summary(self,File):
         #calculate accuracy
@@ -42,12 +44,13 @@ class ConfusionMatrix(object):
         File.write("the model accuracy is {}".format(self.acc)+"\n")
 
         #precision,recall,specificity
-        table=PrettyTable() #创建一个表格
+        table=PrettyTable() # create a table
         table.field_names=["","Precision","Sensitivity","Specificity"]
         for i in range(self.num_classes):
             TP=self.matrix[i,i]
-            FP = np.sum(self.matrix[:, i]) - TP
-            FN = np.sum(self.matrix[i, :]) - TP
+            FP = np.sum(self.matrix[i, :]) - TP
+            FN = np.sum(self.matrix[:, i]) - TP
+            
             TN = np.sum(self.matrix) - TP - FP - FN
 
             Precision=round(TP/(TP+FP),4) if TP+FP!=0 else 0.
@@ -63,26 +66,25 @@ class ConfusionMatrix(object):
         File.write(str(table)+'\n')
         return self.acc
 
-    def plot(self):#绘制混淆矩阵
+    def plot(self):#plot matrix
         matrix=self.matrix
         print(matrix)
         plt.imshow(matrix,cmap=plt.cm.Blues)
 
-        #设置x轴坐标label
+        #x label
         plt.xticks(range(self.num_classes),self.labels,rotation=45)
-        #设置y轴坐标label
+        #y label
         plt.yticks(range(self.num_classes),self.labels)
-        #显示colorbar
+        #show colorbar
         plt.colorbar()
         plt.xlabel('True Labels')
         plt.ylabel('Predicted Labels')
         # plt.title('Confusion matrix (acc='+self.summary()+')')
 
-        #在图中标注数量/概率信息
+       
         thresh=matrix.max()/2
         for x in range(self.num_classes):
             for y in range(self.num_classes):
-                #注意这里的matrix[y,x]不是matrix[x,y]
                 info=int(matrix[y,x])
                 plt.text(x,y,info,
                          verticalalignment='center',
@@ -103,7 +105,7 @@ def calculate_auc(pro_list,lab_list,classnum,File):
     lab_onehot.scatter_(dim=1, index=lab_tensor, value=1)
     lab_onehot = np.array(lab_onehot)
 
-    table = PrettyTable()  # 创建一个表格
+    table = PrettyTable()
     table.field_names = ["", "auc"]
     roc_auc = []
     for i in range(classnum):
